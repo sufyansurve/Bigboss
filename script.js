f660552e7b8f7d36e43583fb1bc4237afd3c2702
sufyansurve <62959355+sufyansurve@users.noreply.github.com>
2023-10-28 22:36:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+  var video = document.querySelector('#player');
+
+  if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource('https://prod-sports-north-gm.jiocinema.com/bpk-tv/Colors_HD_voot_MOB/Fallback/index.m3u8');
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+      video.play();
+    });
+  }
+  
+  plyr.setup(video);
+})();
